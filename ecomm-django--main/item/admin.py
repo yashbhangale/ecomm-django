802266046fc68f619140dcfde88c007ed743e267
yashbhangale <yashbhangale9@gmail.com>
2023-03-12 05:56:04 -0700

    @

---
+++ ecomm-django--main/item/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Category, Item
+
+admin.site.register(Category)
+admin.site.register(Item)
+
+
+
+
+
+
+
+
+
+
+
+
