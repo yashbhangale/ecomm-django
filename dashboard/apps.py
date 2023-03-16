852adbe179fe6d1fe2f54f8c0e28d2fcd7ebc3ff
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-03-16 17:44:52 +0530

    Add files via upload

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ItemConfig(AppConfig):
+class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'item'
+    name = 'dashboard'
--- item/models.py
+++ item/models.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=255)
-
 
     class Meta:
         ordering = ('name',)
@@ -15,15 +12,14 @@ class Category(models.Model):
         return self.name
 
 class Item(models.Model):
-    Category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-   
-
+    
     def __str__(self):
         return self.name
