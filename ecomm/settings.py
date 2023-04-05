bb0390774a516525588e5386aab1fcf8cacb23fb
Yash Bhangale <68957369+yashbhangale@users.noreply.github.com>
2023-04-05 11:33:29 +0530

    Add files via upload

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tc@a#wv-j+l&5on=h8ay014_rxc8kuocs37h0g(=mkjm6u$wo0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1']
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- ecomm/urls.py
+++ ecomm/urls.py
@@ -1,12 +1,7 @@
-from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-from django.views.static import serve
-
-
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -14,6 +9,4 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
